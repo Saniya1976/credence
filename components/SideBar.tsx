04bb28e91cae8b64b82fda9f1d09b71faf44c291
Saniya1976 <saniyakumari1976@gmail.com>
2025-11-01 21:37:50 +0530

    sidebar component

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { sidebarLinks } from "../constants";
+import { cn } from "../lib/utils";
 
-const SideBar = () => {
+const SideBar = ({ user }: SiderbarProps) => {
   return (
-    <div>SideBar</div>
-  )
-}
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={38}
+            height={38}
+            alt="Credence Logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Credence</h1>
+        </Link>
 
-export default SideBar
+        {sidebarLinks.map((item) => (
+          <Link href={item.route} key={item.label} className={cn('sidebar-link')}>
+            {item.label}
+          </Link>
+        ))}
+      </nav>
+    </section>
+  );
+};
+
+export default SideBar;
