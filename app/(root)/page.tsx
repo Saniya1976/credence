597301b8174e3cf070601774a3c7f9865952ff80
Saniya1976 <saniyakumari1976@gmail.com>
2025-11-01 12:28:06 +0530

    layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import HeaderBox from '../../components/HeaderBox'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const Home = () => {
+    const loggedIn = { username: 'Sanya' }
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type='greeting'
+                        title='Welcome '
+                        user={loggedIn?.username || 'Guest'}
+                        subtitle='We are glad to have you here. Explore our services and enjoy a seamless banking experience.'
+
+
+
+                    />
+                </header>
+            </div>
+        </section>
+    )
 }
 
-export default page
+export default Home
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const HeaderBox = ({ type = "title", title, subtitle, user }: HeaderBoxProps) => {
+  return (
+    <div>H</div>
+  )
+}
+
+export default HeaderBox;
